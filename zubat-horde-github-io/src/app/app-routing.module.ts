aa7ac0e3703f1dbd7ce0a1f7f5735ae9d72f3119
Christian Jensen <>
2019-10-27 11:06:43 -0500

    removed template and added nav-menu component

--- zubat-horde-github-io/src/app/app-routing.module.ts
+++ zubat-horde-github-io/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'about-me',
+    component: AboutMeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- zubat-horde-github-io/src/app/app.module.ts
+++ zubat-horde-github-io/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutMeComponent
+    AboutMeComponent,
+    NavMenuComponent
   ],
   imports: [
     BrowserModule,
