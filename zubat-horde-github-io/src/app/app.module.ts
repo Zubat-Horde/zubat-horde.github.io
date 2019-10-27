cb3a1c65ddbaad592e2f32c4d601d23c6730d673
Christian Jensen <>
2019-10-27 10:47:36 -0500

    added about me component

--- zubat-horde-github-io/src/app/app.module.ts
+++ zubat-horde-github-io/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
